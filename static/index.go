da8edbab164a47041149d285b6ab794ad29166be
ken-morel <engonken8@gmail.com>
2024-08-08 07:19:54 +0100

    created static

--- static/index.go
+++ static/index.go
@@ -198,9 +198,6 @@ func MuxServer(a *glance.Application) (*http.ServeMux, error) {
   // TODO: add HTTPS support
   mux := http.NewServeMux()
 
-  mux.HandleFunc("GET /api/{$}", a.HandlePageRequest)
-  mux.HandleFunc("GET /api/{page}", a.HandlePageRequest)
-  mux.HandleFunc("GET /api/pages/{page}/content/{$}", a.HandlePageContentRequest)
   mux.Handle("GET /static/{path...}", http.StripPrefix("/static/", glance.FileServerWithCache(http.FS(assets.PublicFS), 2*time.Hour)))
 
   if a.Config.Server.AssetsPath != "" {
