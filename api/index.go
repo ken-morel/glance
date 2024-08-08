be211cdd8b5f5c23ece6f0b88bd00c2b2afecf3b
ken-morel <engonken8@gmail.com>
2024-08-08 07:01:27 +0100

    testing /api/

--- api/index.go
+++ api/index.go
@@ -198,10 +198,10 @@ func MuxServer(a *glance.Application) (*http.ServeMux, error) {
 	// TODO: add HTTPS support
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /{$}", a.HandlePageRequest)
-	mux.HandleFunc("GET /{page}", a.HandlePageRequest)
+	mux.HandleFunc("GET /api/{$}", a.HandlePageRequest)
+	mux.HandleFunc("GET /api/{page}", a.HandlePageRequest)
 	mux.HandleFunc("GET /api/pages/{page}/content/{$}", a.HandlePageContentRequest)
-	mux.Handle("GET /static/{path...}", http.StripPrefix("/static/", glance.FileServerWithCache(http.FS(assets.PublicFS), 2*time.Hour)))
+	mux.Handle("GET /api/static/{path...}", http.StripPrefix("/static/", glance.FileServerWithCache(http.FS(assets.PublicFS), 2*time.Hour)))
 
 	if a.Config.Server.AssetsPath != "" {
 		absAssetsPath, err := filepath.Abs(a.Config.Server.AssetsPath)
